432fef62c6491afd0c4f5ed220e2886c68627833
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2020-03-04 20:28:15 +0100

    remove comments, modify path for map and create new repo

--- laby.py
+++ laby.py
@@ -29,7 +29,7 @@ class Laby:
         it includes the starting position and the exit
         and finally a method to get random position"""
         
-        with open("maps/path.txt", "r") as f:
+        with open("maps/map1.txt", "r") as f:
             for ligne_n, ligne in enumerate(f):
                 self.hauteur = ligne_n
                 for col_n, col in enumerate(ligne):
@@ -42,17 +42,13 @@ class Laby:
                         self.start.append((ligne_n, col_n))
                     elif col == "X":
                         self.end.append((ligne_n, col_n))
-                                              
-        # -tc- tu peux même déplacer la création de positions aléatoire ici, dès que la liste
-        # -tc- paths a été initialisée.
+                        
         self._random_positions = iter(random.sample(self.paths, len(self.paths)))
                                         
-    # -tc- encore mieux d'en faire une méthode
     def get_random_position(self):
         """Generate random position
         for the laby so path always changed"""
-        # -tc- si self._random_positions est créer sous forme d'un itérateur, il te suffit d'utiliser next()
-        # -tc- pour retourner à chaque fois la prochaine position aléatoire
+        
         return next(self._random_positions)
     
 class Hero:
@@ -101,7 +97,6 @@ class Item:
 Item()
 print(Item().items)
 
-# -tc- Pour le code de test, prendre l'habitude d'utiliser une fonction également
 def test_laby_works_as_expected():
     laby = Laby()
     laby.read_from_file()
@@ -112,7 +107,6 @@ def test_laby_works_as_expected():
     print("largeur: ", laby.largeur )
     print("hauteur: ", laby.hauteur )
    
-# -tc- et prendre l'habitude de le lancer uniquement après cette condition (voir cours "Perfectionnez-vous en python")
 if __name__ == "__main__":
     test_laby_works_as_expected()
     test_hero_works_as_expected()
