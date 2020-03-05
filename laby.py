6a2decf27e4ea2deea9ba871354a0260e0886a1c
Gaëtan GROND <gaetan.grond@gmail.com>
2020-03-05 19:31:27 +0100

    Update laby.py

--- laby.py
+++ laby.py
@@ -1,8 +1,7 @@
 #! /usr/bin/env python3
-"""Main file for the MacGayver Labyrinth game
-MacGayver Labybinth is a game where one character
-has to pick up 3 items and reach the exit 
-in order to succeed"""
+"""file for the MacGayver Labyrinth game
+that handle the laby and the methods 
+link to it"""
 
 import random
 
@@ -10,14 +9,14 @@ class Laby:
     """Laby: Class that represent the play zone
     and the labyrinth
     """
-    def __init__(self, largeur, hauteur):
+    def __init__(self):
         """initialization of instances for the laby class """
 
         self.walls = []
         self.paths = []
         self.start = []
-        self.largeur = largeur
-        self.hauteur = hauteur
+        self.largeur = 0
+        self.hauteur = 0
         self.end = []
         self._random_positions = None
  
@@ -27,6 +26,7 @@ class Laby:
         or path (authorized path) with index and value
         it includes the starting position and the exit
         and finally a method to get random position"""
+
         try:
           with open("maps/map1.txt", "r") as f:
             for ligne_n, ligne in enumerate(f):
@@ -51,21 +51,15 @@ class Laby:
         
         return next(self._random_positions)
 
-    def __str__(self):
-      return f"test"
-
-    
-class Hero(Laby):
+class Hero():
     """Hero: Class hero that set some actions
     (attributs) to Macgayver and
     manage the inventory """
 
-    # -tc- probablement que MacGyver devrait recevoir une instance de Laby en argument
-    # -tc- de son constructeur.
     def __init__(self):
-        # -tc- La position de départ est donnée par l'attribut start de laby
-        Laby.__init__(self)
         self.position= (0, 0)
+        self.pos_x = 0
+        self.pos_y = 0
         self.inventory = 0
         self.won = False
 
@@ -76,49 +70,56 @@ class Hero(Laby):
         we load the class  """
         
         self.inventory += 1             
-        if self.inventory == 3: # -tc- Non, car il faut rejoindre la sortie avant de gagner
+        if self.inventory == 3 and self.position == "X": 
             print("You won !")
             self.won = True
         else:
             print("Not so fast !", self.inventory  ,"item(s) picked up, you need 3.")
 
-   def move(self, direction):
-       
-        if direction == 'down' and self.paths == "#":
-          self.pos_x += 1
-          print("Move down")
+    def move(self, direction):
+        """Method used to move the hero
+        in the labyrinth and test if it is
+        authorized path or not """
+        
+        if direction == 'down':
+            self.pos_x += 1
+            print("Move down")
 
-        elif direction == 'up' and self.paths == "#":
-          self.pos_x -= 1
-          print("Move up")
+        elif direction == 'up':
+            self.pos_x -= 1
+            print("Move up")
 
-        elif direction == 'left' and self.paths == "#":
-          self.pos_y -= 1
-          print("Move left")
+        elif direction == 'left':
+            self.pos_y -= 1
+            print("Move left")
 
-        elif direction == 'right' and self.paths == "#":
-          self.pos_y += 1
-          print("Move right")
+        elif direction == 'right':
+            self.pos_y += 1
+            print("Move right")
 
-def test_hero_works_as_expected(laby):
+def test_hero_works_as_expected():
     """Function that test if Class Hero is creating
     new instances and if the condition is working """
     Hero()
     h = Hero()
-    print(h)
+    h.move("right")
+    h.move("left")
+    h.move("left")
+    h.move("left")
+    h.move("down")
+    print(h.pos_x, h.pos_y)
   
 
 class Item:
     """Item: To generate new items in a 
     random position and incremente 
     Macgayver's inventory """
-    
-    # -tc- Une classe Item représente un item, pas 3
+ 
     def __init__(self, item):
         self.items = item
 
 def test_laby_works_as_expected():
-    laby = Laby(hauteur=0, largeur=0)
+    laby = Laby()
     laby.read_from_file()
     print("Départ: ", laby.start, "Exit: ", laby.end)
     print(laby.get_random_position())
@@ -128,5 +129,5 @@ def test_laby_works_as_expected():
     print("hauteur: ", laby.hauteur )
    
 if __name__ == "__main__":
-  test_laby_works_as_expected()
+  
   test_hero_works_as_expected()
