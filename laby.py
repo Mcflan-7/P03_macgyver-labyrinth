30dfe9a62c758b0b080a90ac6c44c6214ba3b79e
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2020-03-05 17:45:27 +0100

    Update laby.py

--- laby.py
+++ laby.py
@@ -6,24 +6,18 @@ in order to succeed"""
 
 import random
 
-# -tc- Je ne pense pas que Laby soit la classe principale du jeu qui sera plutôt la 
-# -tc- la classe Game ou Application. Laby a comme unique responsabilité de
-# -tc- représenter le labyrinthe et sa structure.
 class Laby:
-    """Laby: Main class for the game
-    Create several objects for the game
+    """Laby: Class that represent the play zone
+    and the labyrinth
     """
-    
-    def __init__(self):
+    def __init__(self, largeur, hauteur):
         """initialization of instances for the laby class """
 
         self.walls = []
         self.paths = []
         self.start = []
-        # -tc- pourquoi une liste? La largeur (width) est un entier
-        self.largeur = []
-        # -tc- pourquoi une liste? La hauteur (height) est un entier
-        self.hauteur = []
+        self.largeur = largeur
+        self.hauteur = hauteur
         self.end = []
         self._random_positions = None
  
@@ -33,8 +27,8 @@ class Laby:
         or path (authorized path) with index and value
         it includes the starting position and the exit
         and finally a method to get random position"""
-        
-        with open("maps/map1.txt", "r") as f:
+        try:
+          with open("maps/map1.txt", "r") as f:
             for ligne_n, ligne in enumerate(f):
                 self.hauteur = ligne_n
                 for col_n, col in enumerate(ligne):
@@ -47,24 +41,30 @@ class Laby:
                         self.start.append((ligne_n, col_n))
                     elif col == "X":
                         self.end.append((ligne_n, col_n))
-                        
-        self._random_positions = iter(random.sample(self.paths, len(self.paths)))
+          self._random_positions = iter(random.sample(self.paths, len(self.paths)))
+        except:
+          print("Map not loaded")      
                                         
     def get_random_position(self):
         """Generate random position
         for the laby so path always changed"""
         
         return next(self._random_positions)
+
+    def __str__(self):
+      return f"test"
+
     
-class Hero:
+class Hero(Laby):
     """Hero: Class hero that set some actions
     (attributs) to Macgayver and
     manage the inventory """
 
     # -tc- probablement que MacGyver devrait recevoir une instance de Laby en argument
     # -tc- de son constructeur.
-    def __init__(self, laby):
+    def __init__(self):
         # -tc- La position de départ est donnée par l'attribut start de laby
+        Laby.__init__(self)
         self.position= (0, 0)
         self.inventory = 0
         self.won = False
@@ -81,23 +81,24 @@ class Hero:
             self.won = True
         else:
             print("Not so fast !", self.inventory  ,"item(s) picked up, you need 3.")
-   
-    def move(self): # -tc- la méthode move doit probablement prendre en paramètre la direction du déplacement
+
+    def right(self):
+      pass
+
+    def move(self, laby): # -tc- la méthode move doit probablement prendre en paramètre la direction du déplacement
         """Method used to move MacGayver
         on the map """
         new_move = self.position # -tc- self.position donne la position actuelle, pas la nouvelle 
         if new_move in self.laby.paths: # -tc- du coup, il faut que ton constructeur enregistre laby en attribut
             self.position = new_move
 
-def test_hero_works_as_expected():
+def test_hero_works_as_expected(laby):
     """Function that test if Class Hero is creating
     new instances and if the condition is working """
     Hero()
     h = Hero()
-    h.pick_up_item()
-    h.pick_up_item() 
-    h.pick_up_item() 
-    print("Succeed: ", h.won)
+    print(h)
+  
 
 class Item:
     """Item: To generate new items in a 
@@ -105,14 +106,11 @@ class Item:
     Macgayver's inventory """
     
     # -tc- Une classe Item représente un item, pas 3
-    def __init__(self):
-        self.items = ["Ether", "Needle", "Syringe"]
-    
-Item()
-print(Item().items)
+    def __init__(self, item):
+        self.items = item
 
 def test_laby_works_as_expected():
-    laby = Laby()
+    laby = Laby(hauteur=0, largeur=0)
     laby.read_from_file()
     print("Départ: ", laby.start, "Exit: ", laby.end)
     print(laby.get_random_position())
@@ -122,5 +120,5 @@ def test_laby_works_as_expected():
     print("hauteur: ", laby.hauteur )
    
 if __name__ == "__main__":
-    test_laby_works_as_expected()
-    test_hero_works_as_expected()
+  test_laby_works_as_expected()
+  test_hero_works_as_expected()
