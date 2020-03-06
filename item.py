6faac5dcefc097f7f074d5e9c6ad2be43f06f063
Gaëtan GROND <gaetan.grond@gmail.com>
2020-03-06 20:25:31 +0100

    add modules for each class

---
+++ item.py
@@ -0,0 +1,7 @@
+class Item:
+    """Item: To generate new items in a 
+    random position and incremente 
+    Macgayver's inventory """
+ 
+    def __init__(self, item):
+        self.items = item
---
+++ launcher.py
@@ -0,0 +1,8 @@
+
+import going
+import laby
+import hero
+ 
+   
+if __name__ == "__main__":
+  pass
